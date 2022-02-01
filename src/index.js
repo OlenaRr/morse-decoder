9555851b29cbabd449e11bffdb2c53952e4ceaca
OlenaRr <olenkar093@gmail.com>
2022-02-01 21:48:46 +0200

    feat: corrected to fit Node 14

--- src/index.js
+++ src/index.js
@@ -40,11 +40,18 @@ const MORSE_TABLE = {
 function decode(expr) {
     let res = "";
     for (let i = 0; i < expr.length; i += 10) {
-        let letter = expr.slice(i,i+10);
+        let letter = expr.slice(i, i+10);
         for (let k = 0; k < letter.length; k++) {
             if (letter[k] !== '0') {letter = letter.slice(k,10); break;}
         }
-        letter =  letter.replaceAll('10','.').replaceAll('11','-');
+        // letter =  letter.replaceAll('10','.').replaceAll('11','-');
+        let new_letter = "";
+        for (let z = 0; z < letter.length; z += 2) {
+            let ch = letter.slice(z, z+2);
+            if (ch === '10') {new_letter += '.';}
+            else if (ch === '11') {new_letter += '-';}
+        }
+        letter = new_letter;
         if (MORSE_TABLE[letter] === undefined) {res += ' ';}
         else {res += MORSE_TABLE[letter];}
     }
